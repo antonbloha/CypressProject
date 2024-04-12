190867bcaaebbf29dd0dde5f8ef725aa0bdb9911
tonybloha <tonybloha@outlook.com>
2024-04-11 18:32:10 -0700

    changes22

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -4,7 +4,7 @@
 describe('firstTest', () => {
   it('Should load home page', () => {
     cy.visit('https://demoblaze.com/')
-    cy.getById('#nava').should('be.visible')
+    cy.get('#nava').should('be.visible')
     cy.get('#login2').click()
     cy.get('#loginusername').type('test', {force:true})
     cy.get('#loginpassword').type('test', {force:true})
