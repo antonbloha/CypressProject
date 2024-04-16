d78259c318020b1c71e2a416840f568c6eab9df0
Art <derevarte@gmail.com>
2024-04-16 10:18:39 -0700

    first commit

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -2,7 +2,7 @@
 
 
 describe('firstTest', () => {
-  it('Should load home page', () => {
+  it.skip('Should load home page', () => {
     cy.visit('https://demoblaze.com/')
     cy.get('#nava').should('be.visible')
     cy.get('#login2').click()
@@ -10,4 +10,9 @@ describe('firstTest', () => {
     cy.get('#loginpassword').type('test', {force:true})
     cy.get('button.btn-primary').contains('Log in').click()
   })
+
+  it('Verify that navigation bar has appeared', () => {
+    cy.visit('https://demoblaze.com/')
+    cy.get('#navbarExample').should('be.visible')
+  });
 })
