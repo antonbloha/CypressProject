ff7b0c8776b4c761f1fb5061eb7999ef45e4e371
tonybloha <tonybloha@outlook.com>
2024-04-16 17:44:21 -0700

    fixed locator

--- cypress/e2e/thirdTest.cy.js
+++ cypress/e2e/thirdTest.cy.js
@@ -3,7 +3,7 @@
 describe('Cypress Clearing Fields', () => {
     it('Type and Clear', () => {
       cy.visit('https://practice-automation.com/form-fields/')
-      cy.getById('#nam').type('Stepan')
+      cy.get('#name').type('Stepan')
       cy.wait(2000)
       cy.getById('#name').clear().type('Vasya')
     })
